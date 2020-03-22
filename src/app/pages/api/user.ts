c2da8363fb72e4516eaafe0d7143ba8a70bca2de
sotaryoutarou <sotaryoutarou@gmail.com>
2020-03-23 07:50:59 +0900

    作り直し

--- src/app/pages/api/user.ts
+++
@@ -1,6 +0,0 @@
-export default async(req, res) => {
-  // 外部APIを呼び出し
-  const response = await fetch("http://next-docker-tutorial_web_1:8081/user.php");
-  const data = await response.json();
-  res.status(200).json(data);
-}
